09c898f188039f4dbfed8c67f7a74423f87e3224
Rafed Ramzi <rafedramzi@gmail.com>
2020-11-29 00:42:03 +0700

    Add s and esc keyboard shortcut

--- content/static/js/keyboard.js
+++ content/static/js/keyboard.js
@@ -6,27 +6,48 @@
  */
 
 // This file implements the behavior of the keyboard shortcut which allows
-// for users to press 'y' to to change browser URL to the canonical URL
-// without triggering a reload.
+// for users to:
+// - press 'y' to to change browser URL to the canonical URL without triggering
+// a reload.
+// - press 'esc' set focus ouf of input element.
+// - press 's' to set focus at search box.
 
-const canonicalURLPath = document.querySelector('.js-canonicalURLPath').dataset['canonicalUrlPath'];
-if (canonicalURLPath && canonicalURLPath !== '') {
-  document.addEventListener('keydown', e => {
-    // TODO(golang.org/issue/40246): consolidate keyboard shortcut behavior across the site.
-    const t = e.target.tagName;
-    if (t === 'INPUT' || t === 'SELECT' || t === 'TEXTAREA') {
+const canonicalURLPath = document.querySelector(".js-canonicalURLPath") && document.querySelector(".js-canonicalURLPath").dataset[
+  "canonicalUrlPath"
+];
+
+document.addEventListener("keydown", (e) => {
+  // TODO(golang.org/issue/40246): consolidate keyboard shortcut behavior across the site.
+  const t = e.target.tagName;
+  switch (t) {
+    case "INPUT":
+      switch (e.key) {
+        case "Esc":
+        case "Escape":
+          e.target.blur();
+          e.preventDefault();
+          return;
+      }
+    case "SELECT":
+    case "TEXTAREA":
       return;
-    }
-    if (e.target.isContentEditable) {
-      return;
-    }
-    if (e.metaKey || e.ctrlKey) {
-      return;
-    }
-    switch (e.key) {
-      case 'y':
-        window.history.replaceState(null, '', canonicalURLPath);
-        break;
-    }
-  });
-}
+  }
+  if (e.target.isContentEditable) {
+    return;
+  }
+  if (e.metaKey || e.ctrlKey) {
+    return;
+  }
+  switch (e.key) {
+    case "S":
+    case "s":
+      document.querySelector('input[name="q"]').focus()
+      e.preventDefault();
+      break;
+    case "y":
+      if (canonicalURLPath && canonicalURLPath !== "") {
+        window.history.replaceState(null, "", canonicalURLPath);
+      }
+      break;
+  }
+});
